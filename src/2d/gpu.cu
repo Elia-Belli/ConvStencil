6a0a8fdf8ae577e4919fa5120b77acb26cb03e9e
Elia-Belli <elia.belli.2158@gmail.com>
2025-07-26 16:25:30 +0200

    test: offset1

--- src/2d/gpu.cu
+++ src/2d/gpu.cu
@@ -93,7 +93,6 @@ __global__ void kernel2d_fp32 (const float * __restrict__ in, float * __restrict
         wmma::store_matrix_sync(out_frag + warp_offset, acc_frag, TENSOR_CORE_M, wmma::mem_row_major);
 
         int out_base_offset1 = begin + IDX(HALO + col / 7, HALO, ldm);
-        int out_base_offset2 = out_base_offset1 + 32;
 
         if(tid % 32 == 0){
             #pragma unroll
@@ -105,7 +104,7 @@ __global__ void kernel2d_fp32 (const float * __restrict__ in, float * __restrict
             #pragma unroll
             for(int i = 8; i < 16; i++) {
                 for(int j = 0; j < 8; j++) {
-                    out[out_base_offset2 + IDX(i, j, 8)] = out_frag[warp_offset + IDX(i, j, TENSOR_CORE_M)];
+                    out[out_base_offset1 + IDX(i, j, 8)] = out_frag[warp_offset + IDX(i, j, TENSOR_CORE_M)];
                 }
             }
         }   
