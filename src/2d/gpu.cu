0eb2664ec1d2f7ef1b51ac368745bc7fd6bbe920
Elia-Belli <elia.belli.2158@gmail.com>
2025-07-26 18:47:49 +0200

    add: thread offset

--- src/2d/gpu.cu
+++ src/2d/gpu.cu
@@ -96,13 +96,13 @@ __global__ void kernel2d_fp32 (const float * __restrict__ in, float * __restrict
         out_base_offset = begin + IDX(HALO + col / 7, HALO, ldm);
         
         #pragma unroll
-        for(int i = tid; i < 64; i+= totalThreads) {
+        for(int i = tid; i < 64; i+= 32) {
             int row = i / 8;
             int col = i % 8;
             out[out_base_offset + IDX(row, col, 8)] = out_frag[warp_offset + IDX(row, col, TENSOR_CORE_M)];
             out[out_base_offset + IDX(row + 8, col, 8)] = out_frag[warp_offset + IDX(row + 8, col, TENSOR_CORE_M)];
         }
-
+        __syncthreads();
     }
 }
 
